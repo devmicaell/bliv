246bf93f9b0108b32f30bcb2de7e0462dccb7cc4
Micael Kauan Freitas Chagas <micael.chagas@mulher.df.gov.br>
2024-09-10 18:06:36 -0300

    comentarios

--- bliv_portal/perfil/admin.py
+++ bliv_portal/perfil/admin.py
@@ -1,5 +1,5 @@
-from django.contrib import admin
-from . import models
-# Register your models here.
+# from django.contrib import admin
+# from . import models
+# # Register your models here.
 
-admin.site.register(models.Perfil)
+# admin.site.register(models.Perfil)
--- bliv_portal/perfil/forms.py
+++ bliv_portal/perfil/forms.py
@@ -1,20 +1,19 @@
-from django import forms 
-from . import models
-from django.contrib.auth.models import User
+# from django import forms 
+# from . import models
+# from django.contrib.auth.models import User
 
-#class PerfilForm(forms.ModelForm):
-#    class Meta():
-#        model = models.Perfil
-#        fields = "__all__"
-#        exclude = ('usuario')
-#
-#
-#class UserForm(forms.ModelForm):
+# # class PerfilForm(forms.ModelForm):
+# #   class Meta():
+# #       model = models.Perfil
+# #       fields = "__all__"
+# #       exclude = ('usuario')#
+
+
+# class UserForm(forms.ModelForm):
 #    class Meta():
 #        model = User
 #        fields = ('first_name', 'last_name', 'username', 'password', 'email')
-#
-#
+
 #    def clean(self, *args, **kwargs):
 #        data = self.data
 #        cleaned = self.cleaned_data
